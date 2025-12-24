d06dae60dfea21c3ce00efd33fce4cc916c8efbb
Antonio Pardo <apardo@spluca.org>
2025-12-24 17:28:20 +0100

    nickname apardo

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -2,7 +2,7 @@
 
 export const GLOBAL = {
   // Site metadata
-  username: "Antonio Pardo",
+  username: "Antonio Pardo (apardo)",
   rootUrl: "https://apardo.spluca.org",
   shortDescription: "Automation Architect",
   longDescription: "A systems engineer with nearly 25 years of experience",
