1f2682ed987c42e6a79cf0c3ad1fe48fb73a32d2
Antonio Pardo <apardo@spluca.org>
2025-12-25 16:49:55 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,12 @@ import cloudflare from '@astrojs/cloudflare';
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apardo.spluca.org',
-  integrations: [mdx(), sitemap(), sentry(), spotlightjs()],
+  integrations: [mdx(), sitemap(), sentry(), spotlightjs(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
